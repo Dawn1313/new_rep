ba3f5fbcf008d419512ef20e256d4bcf9397cd6b
Shu Cheng <Shu@dhcp-10-132-132-116.uawifi.arizona.edu>
2016-01-30 16:22:34 -0700

    initial commit

---
+++ conditional.py
@@ -0,0 +1,11 @@
+x = 100
+day = "Friday"
+if not (x<=10 and 4<9):
+    print("The day is " + str(x) + ". What a good day") 
+    print("Great")
+elif day == "Friday":
+    print("I like " + day)
+else:
+    print("I don't think this will happen")
+print(type(str(x)))
+    
---
+++ iteration.py
@@ -0,0 +1,4 @@
+books = ["Tales of Two Cities", "Harry Potter", "Lord of the Rings"]
+nums = range(2)
+for n in nums:
+    print (books[n])
